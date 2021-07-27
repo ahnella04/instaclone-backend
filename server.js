f23617859131119ba6aeca57c684c45c03a7d4d5
ahnella04 <chqkq96@gmail.com>
2021-07-28 00:56:54 +0900

    Instaclone Setup

--- server.js
+++ server.js
@@ -1,45 +1,13 @@
-import { ApolloServer, gql } from "apollo-server";
-// const { ApolloServer, gql } = require("apollo-server");
-
-const typeDefs = gql`
-  type Movie {
-    title: String
-    year: Int
-  }
-
-  type Query {
-    movies: [Movie]
-    movie: Movie
-  }
-
-  type Mutation {
-    createMovie(title: String!): Boolean
-    deleteMovie(title: String!): Boolean
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => [],
-    movie: () => ({ title: "Hello", year: 2021 })
-  },
-  Mutation: {
-    createMovie: (_, { title }) => {
-      console.log(args);
-      return true;
-    },
-    deleteMovie: (_, { title }) => {
-      console.log(args);
-      return true;
-    }
-  }
-};
+require("dotenv").config(); // .env 파일에서 정보 읽어오기
+import schema from "./schema.js";
+import { ApolloServer } from "apollo-server";
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+  schema
 });
 
+const PORT = process.env.PORT;
+
 server
-  .listen()
-  .then(() => console.log("Server is running on http://localhost:4000"));
+  .listen(PORT)
+  .then(() => console.log(`Server is running on http://localhost:${PORT}`));
